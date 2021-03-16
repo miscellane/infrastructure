28cdc714218f0dd1b3fe79cd052a024e0dffd25c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-16 23:51:13 +0000

    Update spark/src/main/scala/com/grey/libraries/mysql/LoadData.scala

--- spark/src/main/scala/com/grey/libraries/mysql/LoadData.scala
+++ spark/src/main/scala/com/grey/libraries/mysql/LoadData.scala
@@ -8,15 +8,27 @@ import com.grey.libraries.Connecting
 import scala.util.Try
 import scala.util.control.Exception
 
+
+/**
+  * https://dev.mysql.com/doc/refman/8.0/en/load-data.html
+  */
 class LoadData {
 
   // Connectivity Instance
   val connectivityServices = new Connecting()
 
+
+  /**
+    *
+    * @param databaseString: In this case mysql.{databaseName} string
+    * @param tableVariables: Table related variables
+    * @param dropOnFailure: A boolean that dictates whether or not the table in question is dropped on execution failure
+    * @return
+    */
   def loadData (databaseString: String, tableVariables: Map[String, String], dropOnFailure: Boolean = false): Try[Boolean] = {
 
 
-    val queryString = tableVariables("loadString")
+    val queryString = tableVariables("uploadString")
 
 
     // Database
