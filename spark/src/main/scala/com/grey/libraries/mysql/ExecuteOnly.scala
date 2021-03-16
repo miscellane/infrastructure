076e5194e84db8d0f90fb6facbe454d894137e15
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-16 23:06:18 +0000

    Add spark/src/main/scala/com/grey/libraries/mysql/ExecuteOnly.scala

---
+++ spark/src/main/scala/com/grey/libraries/mysql/ExecuteOnly.scala
@@ -0,0 +1,58 @@
+package com.grey.libraries.mysql
+
+import java.sql.DriverManager
+import java.util.Properties
+
+import com.grey.libraries.Connecting
+
+import scala.util.Try
+import scala.util.control.Exception
+
+class ExecuteOnly {
+
+  // Connectivity Instance
+  val connectivityServices = new Connecting()
+
+  def executeOnly(queryString: String, databaseString: String, setAutoCommit: Boolean = false): Try[Boolean] = {
+
+    // Database
+    val databaseValues: Map[String, String] = connectivityServices.databaseKeys(scala.Array(databaseString))
+
+
+    // Driver
+    Class.forName(databaseValues("driver"))
+
+
+    // Properties
+    val properties = new Properties()
+    properties.setProperty("user", databaseValues("user"))
+    properties.setProperty("password", databaseValues("password"))
+
+
+    // Connection Object
+    val connection = DriverManager.getConnection(databaseValues("url"), properties)
+    connection.setAutoCommit(setAutoCommit)
+
+
+    // Statement Object
+    val statement = connection.createStatement()
+
+
+    // Execute
+    val F: Try[Boolean] = Exception.allCatch.withTry(
+      statement.execute(queryString)
+    )
+    connection.close()
+
+
+    // States
+    if (F.isFailure) {
+      sys.error("Error: " + F.failed.get.getMessage)
+    } else {
+      F
+    }
+
+
+  }
+
+}
